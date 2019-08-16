38b7d86a0af70d3d675edeaeaa7207fc0633e338
Hitalo C. Sousa <hitalocunhadesousa@gmail.com>
2019-08-16 19:32:52 -0300

    first commit

---
+++ AirBnbApp/src/pages/main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+// import MapboxGL from '@mapbox/react-native-mapbox-gl';
+
+// import styles from './styles';
+
+const Main = () => (
+  <View />
+);
+
+export default Main;
